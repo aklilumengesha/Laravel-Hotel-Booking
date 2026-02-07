69255c741afc2bdda2a42cc2ff4fa083783a661e
Aklilu Mengesha <aklilumengesha57@gmail.com>
2026-02-07 21:55:13 +0300

    Add customer views (layout)

---
+++ resources/views/customer/layout/styles.blade.php
@@ -0,0 +1,18 @@
+<link rel="stylesheet" href="{{ asset('dist/css/bootstrap.min.css') }}">
+<link rel="stylesheet" href="{{ asset('dist/css/font-awesome.min.css') }}">
+<!-- FontAwesome 5 for modern icons -->
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css" />
+<link rel="stylesheet" href="{{ asset('dist/css/select2.min.css') }}">
+<link rel="stylesheet" href="{{ asset('dist/css/bootstrap-datepicker.min.css') }}">
+<link rel="stylesheet" href="{{ asset('dist/css/bootstrap-timepicker.min.css') }}">
+<link rel="stylesheet" href="{{ asset('dist/css/bootstrap-tagsinput.css') }}">
+<link rel="stylesheet" href="{{ asset('dist/css/duotone-dark.css') }}">
+<link rel="stylesheet" href="{{ asset('dist/css/dataTables.bootstrap4.min.css') }}">
+<link rel="stylesheet" href="{{ asset('dist/css/iziToast.min.css') }}">
+<link rel="stylesheet" href="{{ asset('dist/css/fontawesome-iconpicker.min.css') }}">
+<link rel="stylesheet" href="{{ asset('dist/css/bootstrap4-toggle.min.css') }}">
+<link rel="stylesheet" href="{{ asset('dist/css/summernote-bs4.css') }}">
+<link rel="stylesheet" href="{{ asset('dist/css/style.css') }}">
+<link rel="stylesheet" href="{{ asset('dist/css/components.css') }}">
+<link rel="stylesheet" href="{{ asset('dist/css/spacing.css') }}">
+<link rel="stylesheet" href="{{ asset('dist/css/custom.css') }}">
