7290e0cc51503c8ec38d574c7583973c4602664b
Aklilu Mengesha <aklilumengesha57@gmail.com>
2026-02-07 21:36:53 +0300

    Add admin slide view

---
+++ resources/views/admin/slide_view.blade.php
@@ -0,0 +1,54 @@
+@extends('admin.layout.app')
+
+@section('heading', 'View Slides')
+
+@section('right_top_button')
+<a href="{{ route('admin.slide.create') }}" class="btn btn-primary"><i class="fa fa-plus"></i> Add New</a>
+@endsection
+
+@section('main_content')
+<div class="section-body">
+    <div class="row">
+        <div class="col-12">
+            <div class="card">
+                <div class="card-body">
+                    <div class="table-responsive">
+                        <table class="table table-bordered" id="example1">
+                            <thead>
+                                <tr>
+                                    <th>SL</th>
+                                    <th>Photo</th>
+                                    <th>Action</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                @foreach($slides as $row)
+                                <tr>
+                                    <td>{{ $loop->iteration }}</td>
+                                    <td>
+                                        <img src="{{ asset('uploads/'.$row->photo) }}" alt="" class="w_200">
+                                    </td>
+                                    <td class="pt_10 pb_10">
+    {{-- Corrected Edit Link --}}
+    <a href="{{ route('admin.slide.edit', $row->id) }}" class="btn btn-primary">Edit</a>
+
+    {{-- Corrected and Secure Delete Button --}}
+    <form action="{{ route('admin.slide.destroy', $row->id) }}" method="POST" style="display:inline-block;">
+        @csrf
+        @method('DELETE')
+        <button type="submit" class="btn btn-danger" onclick="return confirm('Are you sure?');">
+            Delete
+        </button>
+    </form>
+</td>
+                                </tr>
+                                @endforeach
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
